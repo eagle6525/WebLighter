44fbeabdf2c96b0383e62425571010e5674fae7a
Wayne Pichotta <wayne@nonfatmedia.com>
2014-01-02 17:59:26 -0800

    Sandwich when regex matches

---
+++ contentscript.js
@@ -0,0 +1,10 @@
+
+var regex = /by Tracy Pierson @|by Greg Hicks @|by Greg Biggins @/;
+
+// Test the text of the body element against our regular expression.
+if (regex.test(document.body.innerText)) {
+  // The regular expression produced a match, so notify the background page.
+  chrome.extension.sendRequest({}, function(response) {});
+} else {
+  // No match was found.
+}
