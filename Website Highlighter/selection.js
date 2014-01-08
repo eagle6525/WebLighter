5e80caf9c09173fcbe1e735c153b22ed71798738
Wayne Pichotta <wayne@nonfatmedia.com>
2014-01-07 17:35:10 -0800

    Highlight in content script

--- Website Highlighter/selection.js
+++ Website Highlighter/selection.js
@@ -1,10 +1,63 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.method == "getSelection")
-        sendResponse({data: window.getSelection().toString()});
-    else
-        sendResponse({}); // snub them.
-});
-
 console.log('foo before');
-console.log(console.log(window.document.location.href));
+// console.log(console.log(window.document.location.href));
+
+function random_color() {
+
+    var style = 'background: ';
+
+    var r, g, b;
+
+    r = Math.round(Math.random() * 0xFF);
+    g = Math.round(Math.random() * 0xFF);
+    b = Math.round(Math.random() * 0xFF);
+
+    style += 'rgba(' + r + ',' + g + ',' + b + ',1);';
+
+    /* The formula for calculating luminance is taken from
+     * http://www.paciellogroup.com/resources/contrast-analyser.html
+     *
+     * If there are better methods to change, please let me know.
+     */
+     var luminance = (r * 299 + g * 587 + b * 114 ) / 1000;
+
+     if (luminance < 125) {
+        style += 'color: #FFFFFF';
+    } else {
+        style += 'color: #000000';
+    }
+
+    return style;
+}
+
+var delimToken = ", "
+var mods = 'Tracy Pierson, Greg Biggins, DERF18, HITITLONG, Greg Hicks, SamoRed, 11banners, BrandonHuffman, DavidWoods'
+var scouts = 'JoshGershon, EvanDaniels, AnnabelStephan, AnnaHickey'
+var connectedSomehow = 'goUCLA05'
+var teamMembers = 'bretth17, uclaBBD, jacklaso'
+
+// console.log(mods);
+var phrasesToHighlight = mods.split(delimToken);
+console.log(phrasesToHighlight);
+for (var i=0; i < phrasesToHighlight.length; i++) {
+	$(document.body).highlight(phrasesToHighlight[i], random_color());
+}
+
+phrasesToHighlight = scouts.split(delimToken);
+console.log(phrasesToHighlight);
+for (var i=0; i < phrasesToHighlight.length; i++) {
+	$(document.body).highlight(phrasesToHighlight[i], random_color());
+}
+
+phrasesToHighlight = connectedSomehow.split(delimToken);
+console.log(phrasesToHighlight);
+for (var i=0; i < phrasesToHighlight.length; i++) {
+	$(document.body).highlight(phrasesToHighlight[i], random_color());
+}
+
+phrasesToHighlight = teamMembers.split(delimToken);
+console.log(phrasesToHighlight);
+for (var i=0; i < phrasesToHighlight.length; i++) {
+	$(document.body).highlight(phrasesToHighlight[i], random_color());
+}
+
 console.log('foo after');
